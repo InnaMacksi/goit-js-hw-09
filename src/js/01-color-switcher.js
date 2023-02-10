57b543610f4487c34804c18a88e888decf6727cd
Inna Makarenko <macksi@ukr.net>
2023-02-10 23:19:35 +0200

    Created Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.querySelector('body');
+const button = document.querySelectorAll('button');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+button[0].addEventListener('click', onBtnColorChange);
+
+function onBtnColorChange() {
+  button[0].setAttribute('disabled', true);
+  button[1].removeAttribute('disabled');
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnActive() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+button[1].addEventListener('click', () => {
+  button[0].removeAttribute('disabled');
+  button[1].setAttribute('disabled', true);
+
+  clearInterval(timerId);
+});
